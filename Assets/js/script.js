71d44cbed47bf832b082e3826a6f965edae44fe6
Scott Simon <simon_phd@protonmail.com>
2022-09-03 01:55:46 -0500

    Debugged JS, wrote README

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -7,6 +7,7 @@ var apiKey = "b8ecb570e32c2e5042581abd004b71bb";
 for (var i = 0; i < localStorage.length; i++) {
 
 	var city = localStorage.getItem(i);
+
 	// console.log(localStorage.getItem("City"));
 	var cityName = $(".list-group").addClass("list-group-item");
 
@@ -21,10 +22,10 @@ searchButton.click(function () {
 
 	var searchInput = $(".searchInput").val();
 
-	// Variable for current weather working 
+	// Variable for current weather
 	var urlCurrent = "https://api.openweathermap.org/data/2.5/weather?q=" + searchInput + "&Appid=" + apiKey + "&units=imperial";
 
-	// Variable for 5 day forecast working
+	// Variable for 5 day forecast
 	var urlFiveDay = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchInput + "&Appid=" + apiKey + "&units=imperial";
 
 	if (searchInput == "") {
@@ -34,10 +35,12 @@ searchButton.click(function () {
 				url: urlCurrent,
 				method: "GET"
 		}).then(function (response) {
+
 				// list-group append an li to it with just set text
 				// console.log(response.name);
 				var cityName = $(".list-group").addClass("list-group-item");
 				cityName.append("<li>" + response.name + "</li>");
+
 				// Local storage
 				var local = localStorage.setItem(keyCount, response.name);
 				keyCount = keyCount + 1;
@@ -46,6 +49,7 @@ searchButton.click(function () {
 				var currentCard = $(".currentCard").append("<div>").addClass("card-body");
 				currentCard.empty();
 				var currentName = currentCard.append("<p>");
+
 				// .addClass("card-text");
 				currentCard.append(currentName);
 
@@ -53,13 +57,17 @@ searchButton.click(function () {
 				var timeUTC = new Date(response.dt * 1000);
 				currentName.append(response.name + " " + timeUTC.toLocaleDateString("en-US"));
 				currentName.append(`<img src="https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png">`);
+
 				// Add Temp 
 				var currentTemp = currentName.append("<p>");
+
 				// .addClass("card-text");
 				currentName.append(currentTemp);
 				currentTemp.append("<p>" + "Temperature: " + response.main.temp + "</p>");
+
 				// Add Humidity
 				currentTemp.append("<p>" + "Humidity: " + response.main.humidity + "%" + "</p>");
+
 				// // Add Wind Speed: 
 				currentTemp.append("<p>" + "Wind Speed: " + response.wind.speed + "</p>");
 
@@ -71,11 +79,9 @@ searchButton.click(function () {
 						url: urlUV,
 						method: "GET"
 				}).then(function (response) {
-
 						var currentUV = currentTemp.append("<p>" + "UV Index: " + response.value + "</p>").addClass("card-text");
 						currentUV.addClass("UV");
 						currentTemp.append(currentUV);
-						// currentUV.append("UV Index: " + response.value);
 				});
 		});
 
@@ -84,11 +90,13 @@ searchButton.click(function () {
 				url: urlFiveDay,
 				method: "GET"
 		}).then(function (response) {
+
 				// Array for 5-days 
 				var day = [0, 8, 16, 24, 32];
 				var fiveDayCard = $(".fiveDayCard").addClass("card-body");
 				var fiveDayDiv = $(".fiveDayOne").addClass("card-text");
 				fiveDayDiv.empty();
+				
 				// For each for 5 days
 				day.forEach(function (i) {
 						var FiveDayTimeUTC1 = new Date(response.list[i].dt * 1000);
--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# weather-api
-Weather API dashboard 
+# 06 Weather API
+
+Module 6 challenge assignment:
+
+Build a weather dashboard API using javascript, bootstrap, jquery, and moment.js
+
+1. Created index, css, js files 
+2. Wrote psuedocode
+3. Declared variables
+4. Created search button click event
+5. Added APIs for current weather and 5-day forecast 
+6. Wrote conditional statements
+7. Wrote .ajax asynchronous data calls
+8. Tested & debugged app with DevTools
+9. Branded and styled app interface
+10. Updated fonts, colors, and hover
+11. Added comments where appropriate
+12. Wrote README and added screenshot and links
+
+- Screenshot
+
+![image](https://user-images.githubusercontent.com/60651145/188259386-37f9c4f9-7e55-4532-b2e7-92b594103f3f.png)
+
+- URL to deployed application:
+https://ssimonphd.github.io/weather-api/
+
+- URL to GitHub repo:
+https://github.com/SSimonPhd/weather-api
